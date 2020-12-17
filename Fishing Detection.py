989799dedfa0792c878952443532b7f91376d9aa
Ashbringer0926 <62210643+Ashbringer0926@users.noreply.github.com>
2020-12-17 14:26:40 +0800

    Add files via upload

---
+++ Fishing Detection.py
@@ -0,0 +1,147 @@
+from sklearn.externals import joblib
+import pandas as pd
+import tldextract
+import math
+from urllib.parse import urlparse
+import socket
+
+Suspicious_TLD=['zip','cricket','link','work','party','gq','kim','country','science','tk']
+Suspicious_Domain=['luckytime.co.kr','mattfoll.eu.interia.pl','trafficholder.com','dl.baixaki.com.br','bembed.redtube.comr','tags.expo9.exponential.com','deepspacer.com','funad.co.kr','trafficconverter.biz']
+
+featureSet = pd.DataFrame(columns=('domain','entropy','num of hyphen','num of delim','len of domain','num of at',\
+'is IP','presence of Suspicious_TLD','presence of suspicious domain','label'))
+
+
+def countdots(url):
+    return url.count('.')
+
+
+def countdelim(url):
+    count = 0
+    delim = [';', '_', '?', '=', '&']
+    for each in url:
+        if each in delim:
+            count = count + 1
+
+    return count
+
+
+import ipaddress as ip  # works only in python 3
+
+
+def isip(uri):
+    try:
+        if ip.ip_address(uri):
+            return 1
+    except:
+        return 0
+
+
+def isPresentHyphen(url):
+    return url.count('-')
+
+
+def isPresentAt(url):
+    return url.count('@')
+
+
+def get_Ent(domain):
+    # domain = get_Name(domain)
+    tmp_dict = {}
+    domain_len = len(domain)
+    for i in range(0, domain_len):
+        if domain[i] in tmp_dict.keys():
+            tmp_dict[domain[i]] = tmp_dict[domain[i]] + 1
+        else:
+            tmp_dict[domain[i]] = 1
+    shannon = 0
+    for i in tmp_dict.keys():
+        p = float(tmp_dict[i]) / domain_len
+        shannon = shannon - p * math.log(p, 2)
+    return shannon
+
+
+def getFeatures(url, label):
+    result = []
+    url = str(url)
+
+    path = urlparse(url)
+    ext = tldextract.extract(url)
+    domain = ext.domain
+    # add the domain to feature set
+    result.append(domain)
+
+    result.append(get_Ent(domain))
+
+    # checking hyphen in domain
+    result.append(isPresentHyphen(domain))
+
+    result.append(countdelim(domain))
+    # length of domain
+    result.append(len(domain))
+
+    # checking @ in the url
+    result.append(isPresentAt(domain))
+
+    # if IP address is being used as a URL
+    result.append(isip(domain))
+
+    # presence of Suspicious_TLD
+    result.append(1 if ext.suffix in Suspicious_TLD else 0)
+
+    # presence of suspicious domain
+    result.append(1 if '.'.join(ext[1:]) in Suspicious_Domain else 0)
+
+    # result.append(get_ext(path.path))
+    result.append(str(label))
+    return result
+
+    # Yay! finally done!
+
+
+def main(message):
+
+
+    result = pd.DataFrame(columns=('domain','entropy','num of hyphen','num of delim','len of domain','num of at',\
+    'is IP','presence of Suspicious_TLD','presence of suspicious domain','label'))
+
+    results = getFeatures(message, '1')
+    result.loc[0] = results
+    result = result.drop(['domain', 'label'], axis=1).values
+
+    # print(clf.predict(result))
+    return clf.predict(result)
+
+
+if __name__ == '__main__':
+    clf = joblib.load('rfc.pkl')
+    message = ''
+    server = socket.socket()
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('127.0.0.1', 9001))
+    server.listen()
+
+    conn, addr = server.accept()
+    while True:
+
+        c_info = conn.recv(1024).decode('gbk')  # 接受客户端消息并解码
+        print(c_info)
+        print(type(c_info))
+        info = main(c_info)
+        info_send = str(info[0])
+        # info_send = "hello"
+        info_send = info_send.encode('gbk')
+        conn.send(info_send)
+        if c_info == 'bye':  # 当客户端发送bye时，服务端给客户端发送bye并结束循环
+            conn.send(b'bye')
+            break
+    conn.close()
+    server.close()
+
+
+    # i = main("www.baidu.com")
+    # print(i)
+    # print(type(i))
+    # print(type(i[0]))
+    # t = int(i[0])
+    # print(t, type(t))
